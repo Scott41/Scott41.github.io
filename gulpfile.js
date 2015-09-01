cac64e9ee280086afdf69d7ff517132335b2698a
Scott Archer <sarcher@longviewinc.com>
2015-09-01 16:46:19 -0400

    Added CSS post-pressing with autoprefixer plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp'),
     del = require('del'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer-core'),
     minifyCss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     shell = require('shelljs'),
@@ -56,14 +58,16 @@ gulp.task('prod-sass', function(done) {
 });
 
 gulp.task('dev-build', ['clean', 'sass', 'browserify'], function () {
+    var processors = [
+        autoprefixer({browsers: ['last 2 version']})
+    ];
     gulp.src(['app/**','!app/styles/**' , '!app/js', '!app/js/**', '!app/**.js', '!app/templates', '!app/templates/**.html'])
         .pipe(gulp.dest('./dist'));
-    gulp.src('app/styles/*.css')
-        .pipe(gulp.dest('./dist/styles'));
     gulp.src('app/bundle.js')
         .pipe(gulp.dest('./dist'));
-
-
+    gulp.src('app/styles/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist/styles'));
 });
 
 gulp.task('prod-build', function () {
